7f4777f3b9eed9b797483a5c3f3666aaa01b2cfa
Marcelino Luna <marce@binfo.es>
2013-03-04 02:12:17 +0100

    Update sdvideo.js

--- sdvideo.js
+++ sdvideo.js
@@ -1,23 +1,28 @@
 (function() {
     id = window.location.toString();
-    idArray = id.split("/");
-    dominio = idArray[2];
+    divide = id.split("#");
+    url = divide[0];
+    urlArray = url.split("/");
+    dominio = urlArray[2];
     switch (dominio) {
     case "www.dailymotion.com":
     case "dailymotion.com":
-        url = id.split("#");
-        url = url[0].replace("/video/", "/embed/video/") + "?logo=0&autoPlay=1&forcedQuality=hq";
+        url = id.replace("/video/", "/embed/video/") + "?logo=0&autoPlay=1&forcedQuality=hq";
         break;
     case "www.youtube.com":
     case "youtube.com":
-        id = id.replace(/^[^v]+v.(.{11}).*/, "$1");
+        id = url.replace(/^[^v]+v.(.{11}).*/, "$1");
         url = "http://www.youtube.com/watch_popup?v=" + id + "&hl=es&fs=1&vq=hd720&autoplay=1&showinfo=1&rel=0";
         break;
     case "vimeo.com":
-        url = "http://player.vimeo.com/video/" + idArray[3] + "?autoplay=1";
+        url = "http://player.vimeo.com/video/" + urlArray[3] + "?autoplay=1";
         break;
     case "www.livestream.com":
-        url = "http://www.livestream.com/embed/" + idArray[3] + "?autoPlay=true";
+        url = "http://www.livestream.com/embed/" + urlArray[3] + "?autoPlay=true";
+        break;
+    case "www.ustream.tv":
+        url = url.replace("/recorded/", "/embed/recorded/") + "?v=3&autoplay=1";
+        break;
     }
     window.location.href = url;
 })()
