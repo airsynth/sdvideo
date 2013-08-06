af0d060b7e5aaf92b940ae7c4184176d62e81d3a
Marcelino Luna <marce@binfo.es>
2013-08-06 18:25:27 +0200

    Update sdvideo.js

--- sdvideo.js
+++ sdvideo.js
@@ -4,23 +4,25 @@
     url = divide[0];
     urlArray = url.split("/");
     dominio = urlArray[2];
+    dominio = dominio.replace("www.", "");
     switch (dominio) {
-    case "www.dailymotion.com":
-    case "dailymotion.com":
-        url = id.replace("/video/", "/embed/video/") + "?logo=0&autoPlay=1&forcedQuality=hq";
+    case "ted.com":
+        url = "http://embed.ted.com/talks/lang/es/" + urlArray[6];
         break;
-    case "www.youtube.com":
+    case "dailymotion.com":
+        url = url.replace("/video/", "/embed/video/") + "?logo=0&autoPlay=1&forcedQuality=hq";
+        break;
     case "youtube.com":
         id = url.replace(/^[^v]+v.(.{11}).*/, "$1");
         url = "http://www.youtube.com/watch_popup?v=" + id + "&hl=es&fs=1&vq=hd720&autoplay=1&showinfo=1&rel=0";
         break;
     case "vimeo.com":
-        url = "http://player.vimeo.com/video/" + urlArray[3] + "?autoplay=1";
+        url = "http://player.vimeo.com/video/" + urlArray[3] + "?autoplay=1%hd1";
         break;
-    case "www.livestream.com":
+    case "livestream.com":
         url = "http://www.livestream.com/embed/" + urlArray[3] + "?autoPlay=true";
         break;
-    case "www.ustream.tv":
+    case "ustream.tv":
         url = url.replace("/recorded/", "/embed/recorded/") + "?v=3&autoplay=1";
         break;
     }
