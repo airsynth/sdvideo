0341144912b160b3c0a64a07ba67bfce7b43e246
Marcelino Luna <marce@binfo.es>
2014-03-23 08:22:25 +0100

    Adaptation to YouTube changes and conversion to popup

--- sdvideo.js
+++ sdvideo.js
@@ -1,10 +1,8 @@
 (function() {
-    id = window.location.toString();
-    divide = id.split("#");
+    divide = window.location.toString().split("#");
     url = divide[0];
     urlArray = url.split("/");
-    dominio = urlArray[2];
-    dominio = dominio.replace("www.", "");
+    dominio = urlArray[2].replace("www.", "");
     switch (dominio) {
     case "ted.com":
         url = "http://embed.ted.com/talks/lang/es/" + urlArray[6];
@@ -13,8 +11,18 @@
         url = url.replace("/video/", "/embed/video/") + "?logo=0&autoPlay=1&forcedQuality=hq";
         break;
     case "youtube.com":
-        id = url.replace(/^[^v]+v.(.{11}).*/, "$1");
-        url = "http://www.youtube.com/watch_popup?v=" + id + "&hl=es&fs=1&vq=hd720&autoplay=1&showinfo=1&rel=0";
+    case "youtube.googleapis.com":
+        id = getURLParameter("v", url);
+        if (id == null) {
+            if (url.indexOf('/v/') != -1) splitUrl = "/v/";
+            if (url.indexOf('/embed/') != -1) splitUrl = "/embed/";
+            id = url.split(splitUrl)[1].split("/")[0];
+        }
+        getList = getURLParameter("list", url);
+        if (getList != null) {
+            list = "list=" + getList + "&";
+        } else list = "";
+        url = "http://www.youtube.com/embed/" + id + "/?" + list + "autohide=1&theme=light&hl=es&fs=1&vq=hd1080&autoplay=1&showinfo=1&rel=0";
         break;
     case "vimeo.com":
         url = "http://player.vimeo.com/video/" + urlArray[3] + "?autoplay=1%hd1";
@@ -22,9 +30,15 @@
     case "livestream.com":
         url = "http://www.livestream.com/embed/" + urlArray[3] + "?autoPlay=true";
         break;
-    case "ustream.tv":
-        url = url.replace("/recorded/", "/embed/recorded/") + "?v=3&autoplay=1";
-        break;
     }
-    window.location.href = url;
+    ancho = 640;
+    alto = 360;
+    izquierda = screen.width - ancho;
+    arriba = screen.height - alto;
+    parametros = 'width=' + ancho + ',height=' + alto + ',left=' + izquierda + ',top=' + arriba + 'location=no,status=no,toolbar=no';
+    self.close;
+    window.open(url, 'SDVideo', parametros);
 })()
+function getURLParameter(name, url) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null
+}
